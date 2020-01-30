65f332b1d55291ffb68cc60d0821a305633a4f47
Dan McDonald <danmcd@joyent.com>
2020-01-30 10:10:53 -0500

    Missing C. Fraire issue

--- usr/src/lib/libzfs/common/libzfs_dataset.c
+++ usr/src/lib/libzfs/common/libzfs_dataset.c
@@ -1315,7 +1315,7 @@ badlabel:
 			 * --------------------------------------------------
 			 * zoned=on	mountpoint (no)	    mountpoint (yes)
 			 *		sharenfs (no)	    sharenfs (yes)
-			 *		sharesmb (no)	    sharesmb (no)
+			 *		sharesmb (no)	    sharesmb (yes)
 			 *
 			 * zoned=off	mountpoint (yes)	N/A
 			 *		sharenfs (yes)
